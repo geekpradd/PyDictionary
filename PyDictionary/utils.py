fac6b7ba2a2e79d6fda622f958cc0081ac112484
Thomas Ashish Cherian <6967017+PandaWhoCodes@users.noreply.github.com>
2019-03-11 15:08:44 +0530

    Update utils.py

--- PyDictionary/utils.py
+++ PyDictionary/utils.py
@@ -2,5 +2,5 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def _get_soup_object(url, parser="html.parser"):
+def _get_soup_object(url, parser="lxml"):
     return BeautifulSoup(requests.get(url).text, parser)
