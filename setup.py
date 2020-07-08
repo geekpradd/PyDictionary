139e700c688ebe1e3396c01383516f3680739ea2
geekpradd <geekpradd@gmail.com>
2020-07-08 18:20:08 +0530

    update setupfile and travis config

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyDictionary", # Replace with your own username
+    version="2.0.0",
+    author="geekpradd",
+    author_email="pradd@outlook.com",
+    description="A real dictionary module for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geekpradd/PyDictionary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.6',
+    install_requires=[
+        'bs4',
+        'click',
+        'goslate',
+        'requests'
+    ]
+)
