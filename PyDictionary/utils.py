67241db7a8b6596f05db2a1fea9d20fd740036e8
Nicolás Moro <87204904+NMoro811@users.noreply.github.com>
2022-05-06 13:44:36 +0200

    Update utils.py
    Include proxies as request parameter

--- PyDictionary/utils.py
+++ PyDictionary/utils.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def _get_soup_object(url, parser="html.parser"):
+def _get_soup_object(url, user_proxies, parser="html.parser"):
+    if user_proxies is not None:
+        return BeautifulSoup(requests.get(url, proxies=user_proxies).text, parser)
     return BeautifulSoup(requests.get(url).text, parser)
