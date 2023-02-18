8a12af65762718654a734b0f9c63612e7dc5ff39
Kenneth, Cho Kai Hung <107766043+KennethCho-CKH@users.noreply.github.com>
2023-02-18 18:20:59 +0000

    Enhancing Functionality
    Added a docstring to the script function to explain what the script does, as well as an output option to allow the user to choose where the output will be written to (default is console) and a filename option to specify the filename when writing to a file (default is output.txt). Additionally, an if name == 'main': block was added to ensure that the script() function is only called if the script is run directly, and not if it is imported as a module.

--- PyDictionary/script.py
+++ PyDictionary/script.py
@@ -1,15 +1,33 @@
 import click
+
 try:
     from .core import *
 except:
     from core import *
-    
+
 @click.command()
 @click.option('--mode','-m',default="meaning",help="Mode of Script [meaning, antonym, synonym]")
 @click.option('--words', '-w',prompt="Enter words in a string separated by commas")
-def script(words,mode):
-    print("PyDictionary:")
+@click.option('--output','-o',default='console',help="Output format [console, file, database]")
+@click.option('--filename','-f',default='output.txt',help="Output filename")
+def script(words,mode,output,filename):
+    """
+    This script uses PyDictionary to find meanings, antonyms, or synonyms of words.
+    """
     word_values = [w.strip() for w in words.split(',')]
     d = PyDictionary(word_values)
     maps = {"meaning":d.printMeanings,"antonym":d.printAntonyms,"synonym":d.printSynonyms}
-    click.echo(maps[mode]())
+
+    if output == 'console':
+        click.echo(maps[mode]())
+    elif output == 'file':
+        with open(filename, 'w') as f:
+            f.write(maps[mode]())
+        click.echo(f"Output written to {filename}.")
+    elif output == 'database':
+        # Code to write to a database can be added here
+        click.echo("Database output not yet implemented.")
+
+if __name__ == '__main__':
+    script()
+
